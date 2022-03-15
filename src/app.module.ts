d9836e0dd0439a35020ff22738652ac3b70eeb66
nolleh7707 <nolleh7707@gmail.com>
2022-03-15 22:45:47 +0900

    use npmpackage serialize-interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { CatsModule } from './cats/cats.module';
 import { LoggingInterceptor } from './logging.interceptor';
 import { GithubModule } from './github-webhook/github.module';
 import { AppMiddleware } from './app.middleware';
-import { SerializeInterceptor } from 'serialize.interceptor';
+// import { SerializeInterceptor } from 'serialize-interceptor';
 
 // you can use @Global() decorator for module.
 @Module({
@@ -15,7 +15,7 @@ import { SerializeInterceptor } from 'serialize.interceptor';
   controllers: [AppController],
   providers: [
     AppService,
-    SerializeInterceptor,
+    // SerializeInterceptor,
     // alternative global intercepting method, for injection other class
     {
       provide: APP_INTERCEPTOR,
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './app.bootstrap';
 import { middlefunc } from './app.middleware';
-import { SerializeInterceptor } from 'serialize.interceptor'; 
+import { SerializeInterceptor, camelToSnake } from 'serialize-interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   setupSwagger(app);
   app.use(middlefunc);
-  app.useGlobalInterceptors(new SerializeInterceptor());	
+  camelToSnake(null);
+  // app.useGlobalInterceptors(new SerializeInterceptor());	
 
   await app.listen(3000);
 }
