a600cb959258f5071e99a88bfcf71978be79e792
nolleh7707 <nolleh7707@gmail.com>
2023-04-16 09:46:39 +0900

    chore: testing jest-badge-deploy-action

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { setupSwagger } from './app.bootstrap';
 import { middlefunc } from './app.middleware';
 import { SerializeInterceptor } from 'serialize-interceptor';
 // import { AppDataSource } from 'data-source';
-
+// import { CsvParser } from '@nolleh/simple-csv-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -21,6 +21,19 @@ async function bootstrap() {
   //    // here you can start to work with your database
   //  })
   //  .catch((error) => console.log(error));
+  //
+  // class Message {
+  //   name!: string;
+  //   message!: string;
+  //   constructor(payload?: Partial<Message>) {
+  //     Object.assign(this, payload);
+  //   }
+  // }
+  //
+  // const messages = CsvParser.toObject<Message>(
+  //   `name,message\r\nnolleh,"hello, world"\nnolleh,hello2`,
+  // );
+  // console.log({ messages });
   await app.listen(3000);
 }
 
